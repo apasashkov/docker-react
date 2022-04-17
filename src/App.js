391b03b111fffc1348e06af23c6932e295b39098
anton <anton.pasashkov@lexicallabs.com>
2022-04-18 01:12:57 +0200

    update react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hzzxcvxcvxxxxxxxqqqqqqqqaaaaaaaa
+          Hzzzzxxx
         </p>
         <a
           className="App-link"
